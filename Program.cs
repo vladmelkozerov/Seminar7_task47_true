49f15a973fd9bab4412c64ba6eb55c3490425a2c
Влад <seeker13@mail.ru>
2022-10-24 22:56:31 +0500

    2nd commit

--- Program.cs
+++ Program.cs
@@ -6,15 +6,13 @@ void PrintArray (int[,] array)
     {   for (int i=0;i<array.GetLength(0);i++)
             {   for (int j=0;j<array.GetLength(1);j++)
                     Console.Write(" "+array[i,j]);
-            Console.WriteLine();
-            }
+            Console.WriteLine();}
     }
 int [,] FillArray(int[,] array1)
     {  for (int i1=0;i1<array1.GetLength(0);i1++)
             for (int j1=0;j1<array1.GetLength(1);j1++)
                     array1[i1,j1]=new Random().Next(10,20);
-        return array1;
-    }
+        return array1;}
 void SearchElement (int[,] array2)
     {   Console.WriteLine("Введите номер строки искомого элемента массива");
         int i2=Convert.ToInt32(Console.ReadLine());
@@ -23,8 +21,7 @@ void SearchElement (int[,] array2)
         if (0<=i2&&i2<array2.GetLength(0)&&0<=j2&&j2<array2.GetLength(1))
             Console.WriteLine ($"В исходном массиве найден элемент с адресом [{i2}, {j2}], его значение {array2[i2,j2]}");
         else
-            Console.WriteLine ($"В исходном массиве не найден элемент с адресом [{i2}, {j2}]");
-    }
+            Console.WriteLine ($"В исходном массиве не найден элемент с адресом [{i2}, {j2}]");}
 int[,] array3=new int [m,n];
 Console.WriteLine("Исходный массив");
 PrintArray(FillArray(array3));
